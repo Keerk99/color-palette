f36781e7306a5de4eaf41b06ee425c016ca441b7
Keerk99 <antonyhuamantuanama@gmail.com>
2023-10-22 22:36:35 -0500

    Testing some features

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,12 +1,22 @@
 import "./style.css";
+import styled from "styled-components";
+
+const FooterContainer = styled.div`
+  padding: 1rem 2rem;
+  text-align: center;
+  background: #2d2e2e;
+  color: #ffffff;
+`;
 
 export default function Footer() {
   return (
     <footer>
-      <div className="container__footer">
+      {/* <div className="container__footer"> */}
+      <FooterContainer>
         <p className="copyright">© Copyright Developed by Antony Huaman</p>
         <span className="copyright__span">2023</span>
-      </div>
+      </FooterContainer>
+      {/*    </div> */}
     </footer>
   );
 }
